a070a717ee9b03dd29c1e1a131aca742e72659fa
isghj5 <isghj5@gmail.com>
2020-03-26 15:19:43 -0700

    Pulled Gameobjects/Scene and Extensions/SceneExtensions.cs from entrance-logic-check branch to use scene enum

---
+++ MMR.Randomizer/Attributes/Entrance/ExitCutsceneAttribute.cs
@@ -0,0 +1,21 @@
+﻿using MMR.Randomizer.Extensions;
+using MMR.Randomizer.GameObjects;
+using System;
+
+namespace MMR.Randomizer.Attributes.Entrance
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class ExitCutsceneAttribute : Attribute
+    {
+        public int SceneId { get; private set; }
+        public byte SetupIndex { get; private set; }
+        public byte CutsceneIndex { get; private set; }
+
+        public ExitCutsceneAttribute(Scene scene, byte setupIndex, byte cutsceneIndex)
+        {
+            SceneId = scene.Id();
+            SetupIndex = setupIndex;
+            CutsceneIndex = cutsceneIndex;
+        }
+    }
+}
---
+++ MMR.Randomizer/Extensions/SceneExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MMR.Common.Extensions;
+using MMR.Randomizer.Attributes.Entrance;
+using MMR.Randomizer.GameObjects;
+
+namespace MMR.Randomizer.Extensions
+{
+    public static class SceneExtensions
+    {
+        public static byte Id(this Scene scene)
+        {
+            return scene.GetAttribute<SceneInternalIdAttribute>().InternalId;
+        }
+    }
+}
