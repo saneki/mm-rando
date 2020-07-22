fd5d1a74ea8e83476951cf8b7838233d7380e35f
saneki <s@neki.me>
2020-07-22 09:23:59 -0500

    Add version check for armips version 0.11 or later

--- assembly/src/build.asm
+++ assembly/src/build.asm
@@ -1,6 +1,12 @@
 .n64
 .relativeinclude on
 
+;; Force armips version 0.11 or later for fix to MIPS LO/HI ELF symbol relocation.
+.if (version() < 110)
+.notice version()
+.error "Detected armips build is too old. Please install https://github.com/Kingcom/armips version 0.11 or later."
+.endif
+
 .create "../roms/patched.z64", 0
 .incbin "../roms/base.z64"
 
