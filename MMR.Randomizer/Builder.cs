d2eed23c5651848d1569dcbbc68b5de79574d1b7
isghj5 <isghj5@gmail.com>
2020-07-27 15:24:54 -0700

    Music Rando songforce: randomize their order

--- MMR.Randomizer/Builder.cs
+++ MMR.Randomizer/Builder.cs
@@ -131,7 +131,7 @@ namespace MMR.Randomizer
             }
 
             // MORE DEBUG: if the user wants to force a song to always show up each seed, but in random slots
-            List<SequenceInfo> ForcedSequences = RomData.SequenceList.FindAll(u => u.Name.Contains("songforce") == true);
+            List<SequenceInfo> ForcedSequences = RomData.SequenceList.FindAll(u => u.Name.Contains("songforce") == true).OrderBy(x => random.Next()).ToList();
             if (ForcedSequences != null && ForcedSequences.Count > 0)
             {
                 foreach(SequenceInfo seq in ForcedSequences)
