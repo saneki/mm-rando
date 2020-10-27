022afecc0a8425392908d5042920d05461c57f6e
ZoeyZolotova <zoey.zolotova@gmail.com>
2020-10-27 22:08:49 +1100

    Fix compiler complaining about duplicate definitions.

--- assembly/c/gfx.h
+++ assembly/c/gfx.h
@@ -16,11 +16,11 @@ typedef struct {
 } sprite_t;
 
 //sprite_t items_sprite;
-sprite_t dpad_sprite;
-sprite_t font_sprite;
-sprite_t icon_sprite;
-sprite_t icon_24_sprite;
-sprite_t fairy_sprite;
+extern sprite_t dpad_sprite;
+extern sprite_t font_sprite;
+extern sprite_t icon_sprite;
+extern sprite_t icon_24_sprite;
+extern sprite_t fairy_sprite;
 
 void gfx_init();
 sprite_t* gfx_get_item_textures_sprite(void);
