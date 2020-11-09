33075c8248b3da1570e196b90febf5839ba6b8ff
ZoeyZolotova <zoey.zolotova@gmail.com>
2020-11-10 01:25:10 +1100

    Fix VC crash when picking up various items.

--- assembly/src/hooks/mmr_gi.asm
+++ assembly/src/hooks/mmr_gi.asm
@@ -3,15 +3,28 @@
 ; Name: code
 ; =========================================================
 .headersize (0x800A5AC0 - 0x00B3C000)
-; Replaces:
-;   .dw 0x6E745F72 ; ldr     s4, 0x5F72(s3)
-;   .dw 0x65675B38 ; daddiu  a3, t3, 0x5B38
-;   .dw 0x325D0000 ; andi    sp, s2, 0x0000
-;   .dw 0x7765656B ; jalx    0x8D9595AC
-;   .dw 0x5F657665
+; Replaces (. is \x00):
+;   nt_reg[82]..week
+;   _event_reg[83]..
+;   week_event_reg[8
+;   4]..week_event_r
+;   eg[85]..
 .org 0x801DC790
+    addiu   sp, sp, -0x20
+    sw      ra, 0x001C (sp)
+    sw      a0, 0x0018 (sp)
+    sw      a1, 0x0014 (sp)
+    sw      a2, 0x0010 (sp)
+    sw      a3, 0x0008 (sp)
     or      a3, a1, r0
     or      a2, a0, r0
     or      a1, s0, r0
-    j       mmr_GetNewGiIndex
+    jal     mmr_GetNewGiIndex
     or      a0, s6, r0
+    lw      a3, 0x0008 (sp)
+    lw      a2, 0x0010 (sp)
+    lw      a1, 0x0014 (sp)
+    lw      a0, 0x0018 (sp)
+    lw      ra, 0x001C (sp)
+    jr      ra
+    addiu   sp, sp, 0x20
