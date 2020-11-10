0869e0bfb64da0de26ef398b572d68be12a3561d
saneki <s@neki.me>
2020-11-10 09:15:59 -0600

    Cleanup unused code in Sound Check speedup

--- assembly/src/speedups.asm
+++ assembly/src/speedups.asm
@@ -116,22 +116,6 @@ sakon_should_end_thief_escape_hook:
     jr      ra
     addiu   sp, sp, 0x18
 
-toto_should_skip_formal_replay_hook:
-    addiu   sp, sp, -0x18
-    sw      ra, 0x0014 (sp)
-
-    or      a0, s4, r0
-    jal     toto_should_skip_formal_replay
-    or      a1, s3, r0
-
-    ; Displaced code
-    lui     t8, 0x801F
-    addu    t9, s5, s0
-
-    lw      ra, 0x0014 (sp)
-    jr      ra
-    addiu   sp, sp, 0x18
-
 toto_before_advance_formal_replay_hook:
     sw      a1, 0x0024 (sp)
 
