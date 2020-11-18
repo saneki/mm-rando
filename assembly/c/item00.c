fb17297b7cd996d370110403364b2ea207600274
ZoeyZolotova <zoey.zolotova@gmail.com>
2020-11-19 00:50:47 +1100

    Add entries to collectable-table and append entries to gi-table.

--- assembly/c/item00.c
+++ assembly/c/item00.c
@@ -8,7 +8,7 @@ u16 item00_get_gi_index(z2_en_item00_t *actor, z2_game_t *game) {
 
 void item00_constructor(z2_en_item00_t *actor, z2_game_t *game) {
     if (actor->collectable_flag != 0) {
-        u16 collectable_table_index = game->scene_index * 0x80 + actor->collectable_flag;
+        u16 collectable_table_index = game->scene_index * 0x60 + actor->collectable_flag;
         
         u32 index = MISC_CONFIG.collectable_table_file_index;
         z2_file_table_t entry = z2_file_table[index];
