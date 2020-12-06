87e07a108831b99dbed24186227c374645ef493a
saneki <s@neki.me>
2020-12-05 23:22:08 -0600

    Add subprojects MMR.Yaz and MMR.Yaz.CLI with the aim to provide performant Yaz0 encoding/decoding
    This is an implementation of Yaz0 decoding & encoding which attempts to be performant and low-allocation. To achieve this, it makes use of more recent .NET constructs such as Span<T> and ref structs.
    
    This implementation can perform both encode & decode operations without allocating anything on the heap. During encoding, it allows the user to optionally pass in buffers which are used for state, specifically for the "deltas window" and "recents." However, it can also allocate these buffers for the user on either the stack or the heap. If allocating these on the stack, both the decode & encode operations should not cause any heap allocations, with the possible exception of allocating the destination buffer if specified.
    
    The terms "deltas window" and "recents" are specifically used during encoding and are a bit vague, so here is a brief explanation of what they are:
    
    The "deltas window" refers to a buffer which stores deltas to previous indexes, where the value at these indexes matches the value of the current byte. This is used to efficiently "walk backwards" through previous indexes to look for matching strings during a lookup, operating similarly to a linked list.
    
    The "recents" buffer is a mapping of byte values to the index of the most recent byte with that same value. This is used to find where in the "deltas window" the algorithm should start walking.
    
    The subproject MMR.Yaz.CLI is less intended for general use and more for manual testing of the algorithm when needed.

---
+++ MMR.Yaz/Helpers/RevolvingBufferTracker{T}.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MMR.Yaz.Helpers
+{
+    /// <summary>
+    /// Contains state used for tracking revolving buffer.
+    /// </summary>
+    /// <typeparam name="T">Type of element in buffer.</typeparam>
+    ref struct RevolvingBufferTracker<T>
+    {
+        /// <summary>
+        /// Amount of items currently being used.
+        /// </summary>
+        public int Amount { get; private set; }
+
+        /// <summary>
+        /// Origin index of buffer.
+        /// </summary>
+        public int Origin { get; private set; }
+
+        /// <summary>
+        /// Get an element at the specified index in a revolving buffer.
+        /// </summary>
+        /// <param name="self">Revolving buffer tracker.</param>
+        /// <param name="index">Index.</param>
+        /// <param name="buffer">Underlying buffer.</param>
+        /// <returns>Element at specified index.</returns>
+        /// <remarks>Does not check if the gotten byte is in bounds.</remarks>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T Get(ref RevolvingBufferTracker<T> self, int index, Span<T> buffer)
+        {
+            var position = self.Origin + index;
+            if (position < buffer.Length)
+            {
+                return buffer[position];
+            }
+            else
+            {
+                return buffer[position - buffer.Length];
+            }
+        }
+
+        /// <summary>
+        /// Put a given element into a revolving buffer at the specified index.
+        /// </summary>
+        /// <param name="self">Revolving buffer tracker.</param>
+        /// <param name="index">Index.</param>
+        /// <param name="item">Item to put.</param>
+        /// <param name="buffer">Underlying buffer.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Put(ref RevolvingBufferTracker<T> self, int index, T item, Span<T> buffer)
+        {
+            var position = self.Origin + index;
+            if (position < buffer.Length)
+            {
+                buffer[position] = item;
+            }
+            else
+            {
+                buffer[position - buffer.Length] = item;
+            }
+        }
+
+        /// <summary>
+        /// Push an item to the "top" of the revolving buffer.
+        /// </summary>
+        /// <param name="self">Revolving buffer tracker.</param>
+        /// <param name="item">Item to push.</param>
+        /// <param name="buffer">Underlying buffer.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Push(ref RevolvingBufferTracker<T> self, T item, Span<T> buffer)
+        {
+            if (self.Amount < buffer.Length)
+            {
+                // Console.WriteLine("Amount: {0:X4}, Origin: {1:X4}, Length: {2:X4}", self.Amount, self.Origin, buffer.Length);
+                // Buffer is not yet full, overwrite unused item.
+                /// var position = self.Origin + self.Amount;
+                Put(ref self, self.Amount, item, buffer);
+                self.Amount++;
+            }
+            else
+            {
+                // Buffer is full, advance origin and overwrite last byte.
+                Put(ref self, 0, item, buffer);
+                self.Origin = (self.Origin + 1) < buffer.Length ? (self.Origin + 1) : 0;
+            }
+        }
+
+        /// <summary>
+        /// Pop an item from the "top" of the revolving buffer.
+        /// </summary>
+        /// <param name="self">Revolving buffer tracker.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Pop(ref RevolvingBufferTracker<T> self)
+        {
+            if (self.Amount != 0)
+            {
+                self.Amount--;
+            }
+        }
+    }
+}
