c7982fae1229174d41aab964d181b9324dc6e7e6
saneki <s@neki.me>
2020-12-12 07:54:51 -0600

    Polish behavior of ice traps in water
    Frozen player now floats on water if Human or Zora forms, unless standing underwater as Zora.

--- assembly/src/hooks/player_actor.asm
+++ assembly/src/hooks/player_actor.asm
@@ -1,4 +1,34 @@
 ;==================================================================================================
+; Before Handle Player Frozen State
+;==================================================================================================
+
+.headersize (G_PLAYER_ACTOR_VRAM - G_PLAYER_ACTOR_FILE)
+
+; Replaces:
+;   or      s0, a0, r0
+;   or      s1, a1, r0
+;   sw      ra, 0x001C (sp)
+.org 0x808546DC ; RDRAM: 0x80771B6C, Offset: 0x26C4C
+    sw      ra, 0x001C (sp)
+    jal     player_before_handle_frozen_state_hook
+    or      s0, a0, r0
+
+;==================================================================================================
+; Before Handle Player Voiding State
+;==================================================================================================
+
+.headersize (G_PLAYER_ACTOR_VRAM - G_PLAYER_ACTOR_FILE)
+
+; Replaces:
+;   or      s0, a0, r0
+;   or      s1, a1, r0
+;   sw      ra, 0x001C (sp)
+.org 0x80854228 ; RDRAM: 0x807716B8, Offset: 0x26798
+    sw      ra, 0x001C (sp)
+    jal     player_before_handle_void_state_hook
+    or      s0, a0, r0
+
+;==================================================================================================
 ; Should Ice Void Zora
 ;==================================================================================================
 
@@ -17,3 +47,29 @@
     lui     t2, 0x0002       ;;
     beqz    v0, 0x8085479C   ;; If returned false (0), jump past code which sets void flag.
     addu    t2, t2, s1       ;;
+
+;==================================================================================================
+; Should Prevent Restoring Swim State
+;==================================================================================================
+
+.headersize (G_PLAYER_ACTOR_VRAM - G_PLAYER_ACTOR_FILE)
+
+; Fix branch into patched code, jump into the branch (instead of the delay slot).
+; Replaces:
+;   bnel    v1, at, 0x8083BE0C
+.org 0x8083BCC4
+    bnel    v1, at, 0x8083BE08
+
+; Branch to function end early if not restoring swim state.
+; Replaces:
+;   lw      t4, 0xA6C (a1)
+;   sll     t5, t4, 4
+;   bgez    t5, 0x8083BEB4
+;   nop
+;   lb      t6, 0x0145 (a1)
+.org 0x8083BE08 ; RDRAM: 0x80759298
+    jal     player_should_prevent_restoring_swim_state_hook
+    or      a2, v0, r0      ;; A2 = Function pointer.
+    bnez    at, 0x8083BF44  ;; Branch to function end if not restoring swim state.
+    lb      t6, 0x0145 (a1) ;; Displaced code.
+    bgez    t5, 0x8083BEB4  ;; Original branch if swim flag not set.
--- assembly/src/player_actor.asm
+++ assembly/src/player_actor.asm
@@ -14,6 +14,14 @@ before_player_actor_update_hook:
     jr      ra
     addiu   sp, sp, 0x18
 
+player_before_handle_frozen_state_hook:
+    j       player_before_handle_frozen_state
+    or      s1, a1, r0 ;; Displaced code.
+
+player_before_handle_void_state_hook:
+    j       player_before_handle_void_state
+    or      s1, a1, r0 ;; Displaced code.
+
 player_should_ice_void_zora_hook:
     addiu   at, r0, 0x0002      ;; AT = Zora form value.
     bnel    t9, at, @@not_zora  ;; If not Zora, return false early.
@@ -31,3 +39,27 @@ player_should_ice_void_zora_hook:
 @@not_zora:
     jr      ra
     nop
+
+player_should_prevent_restoring_swim_state_hook:
+    lw      a0, 0x0020 (sp) ;; A0 = GlobalContext.
+
+    addiu   sp, sp, -0x20
+    sw      ra, 0x0018 (sp)
+    sw      a1, 0x0010 (sp)
+
+    jal     player_should_prevent_restoring_swim_state
+    sw      v0, 0x0014 (sp)
+
+    ; Move result to AT.
+    or      at, v0, r0
+
+    lw      a1, 0x0010 (sp)
+    lw      v0, 0x0014 (sp)
+
+    ; Displaced code.
+    lw      t4, 0x0A6C (a1)
+    sll     t5, t4, 4
+
+    lw      ra, 0x0018 (sp)
+    jr      ra
+    addiu   sp, sp, 0x20
