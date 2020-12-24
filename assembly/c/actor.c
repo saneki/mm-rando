fb1684b6e7095a3128fd2a429087033def5a0281
saneki <s@neki.me>
2020-12-24 09:47:18 -0600

    Refactor Vec types

--- assembly/c/actor.c
+++ assembly/c/actor.c
@@ -12,6 +12,6 @@ void actor_after_dtor(z2_actor_t *actor, z2_game_t *game) {
     models_after_actor_dtor(actor);
 }
 
-z2_actor_t* actor_spawn(z2_game_t *game, u8 id, z2_xyzf_t pos, z2_rot_t rot, u16 var) {
+z2_actor_t* actor_spawn(z2_game_t *game, u8 id, Vec3f pos, Vec3s rot, u16 var) {
     return z2_SpawnActor(&(game->actor_ctxt), game, id, pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, var);
 }
