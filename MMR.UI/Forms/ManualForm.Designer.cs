89680eaae2f2d44e45d48c1f4ee2c1a558cc0d0a
ZoeyZolotova <zoey.zolotova@gmail.com>
2020-12-26 23:25:34 +1100

    Update Manual Comfort tab.

--- MMR.UI/Forms/ManualForm.Designer.cs
+++ MMR.UI/Forms/ManualForm.Designer.cs
@@ -233,7 +233,7 @@
             // 
             this.label1.Location = new System.Drawing.Point(3, 1);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(377, 1383);
+            this.label1.Size = new System.Drawing.Size(377, 1324);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
