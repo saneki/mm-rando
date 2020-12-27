c422115b9bfd8c35eea08f35a4f65ea63b1d5db1
saneki <s@neki.me>
2020-12-26 21:17:14 -0600

    Refactor Asm/C: Refactor MessageTable file

--- assembly/src/message_table.asm
+++ assembly/src/message_table.asm
@@ -1,4 +1,4 @@
-message_table_get_data_file_vrom_hook:
+MessageTable_GetDataFileVrom_Hook:
     addiu   sp, sp, -0x28
     sw      ra, 0x0024 (sp)
     sw      a2, 0x0010 (sp)
@@ -6,7 +6,7 @@ message_table_get_data_file_vrom_hook:
     sw      t6, 0x0018 (sp)
     sw      t8, 0x001C (sp)
 
-    jal     message_table_get_data_file_vrom
+    jal     MessageTable_GetDataFileVrom
     sw      at, 0x0020 (sp)
 
     lw      a2, 0x0010 (sp)
@@ -18,7 +18,7 @@ message_table_get_data_file_vrom_hook:
     jr      ra
     addiu   sp, sp, 0x28
 
-message_table_is_primary_message_hook:
+MessageTable_IsPrimaryMessage_Hook:
     addiu   sp, sp, -0x30
     sw      ra, 0x0028 (sp)
     sw      a0, 0x0010 (sp)
@@ -29,7 +29,7 @@ message_table_is_primary_message_hook:
     sw      t3, 0x0024 (sp)
 
     lw      a0, 0x0070 (sp) ;; Load GlobalContext from caller stack (reserved).
-    jal     message_table_is_primary_message
+    jal     MessageTable_IsPrimaryMessage
     or      a1, t1, r0      ;; Message Id.
 
     ; Move result to AT
@@ -45,7 +45,7 @@ message_table_is_primary_message_hook:
     jr      ra
     addiu   sp, sp, 0x30
 
-message_table_lookup_extended_hook:
+MessageTable_LookupExtended_Hook:
     lw      a1, 0x0004 (sp)
 
     addiu   sp, sp, -0x20
@@ -53,7 +53,7 @@ message_table_lookup_extended_hook:
     sw      a0, 0x0010 (sp)
     sw      a2, 0x0014 (sp)
 
-    jal     message_table_lookup_extended
+    jal     MessageTable_LookupExtended
     sw      v1, 0x0018 (sp)
 
     ; Move result to T9
