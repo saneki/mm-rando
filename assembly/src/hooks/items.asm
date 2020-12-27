e53ac151bd26067b3ad0481724ac89b74d3b39f1
saneki <s@neki.me>
2020-12-26 20:45:30 -0600

    Refactor Asm/C: Refactor Items file

--- assembly/src/hooks/items.asm
+++ assembly/src/hooks/items.asm
@@ -8,13 +8,13 @@
 ; Replaces:
 ;   jr      ra
 .org 0x801143C4
-    j       items_after_receive_hook
+    j       Items_AfterReceive_Hook
 
 ; Hook after item removal.
 ; Replaces:
 ;   jr      ra
 .org 0x80114A94
-    j       items_after_removal_hook
+    j       Items_AfterRemoval_Hook
 
 ;==================================================================================================
 ; Fix Item Receive for Custom Items
@@ -30,7 +30,7 @@
 ;   addiu   t0, t0, 0xF670
 ;   addu    t9, t0, t3
 .org 0x801143A0
-    jal     items_should_try_write_to_inventory_hook
+    jal     Items_ShouldTryWriteToInventory_Hook
     lui     t0, 0x801F
     beqzl   v0, 0x801143BC
     addiu   v0, r0, 0x00FF
