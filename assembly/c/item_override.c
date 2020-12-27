84a2d3a8967295dc1022365929feb222d585e87a
saneki <s@neki.me>
2020-12-26 20:38:39 -0600

    Refactor Asm/C: Refactor ItemOverride file

--- assembly/c/item_override.c
+++ assembly/c/item_override.c
@@ -3,13 +3,13 @@
 
 #define ITEM_OVERRIDE_ENTRY_COUNT 0x200
 
-struct item_override_entry {
+struct ItemOverrideEntry {
     u16 graphic;
     u16 object;
 };
 
 // Item graphic override table, used by ice traps to mimic other items.
-struct item_override_entry ITEM_OVERRIDE_ENTRIES[ITEM_OVERRIDE_ENTRY_COUNT] = { 0 };
+struct ItemOverrideEntry ITEM_OVERRIDE_ENTRIES[ITEM_OVERRIDE_ENTRY_COUNT] = { 0 };
 
 // Full size of item graphic override table.
 u32 ITEM_OVERRIDE_COUNT = ITEM_OVERRIDE_ENTRY_COUNT;
@@ -17,9 +17,9 @@ u32 ITEM_OVERRIDE_COUNT = ITEM_OVERRIDE_ENTRY_COUNT;
 /**
  * Helper function to retrieve the item graphic override values for a specific get-item index.
  **/
-bool item_override_get_graphic(u32 index, u16 *graphic, u16 *object) {
+bool ItemOverride_GetGraphic(u32 index, u16* graphic, u16* object) {
     if (index < ITEM_OVERRIDE_ENTRY_COUNT) {
-        struct item_override_entry entry = ITEM_OVERRIDE_ENTRIES[index];
+        struct ItemOverrideEntry entry = ITEM_OVERRIDE_ENTRIES[index];
         if (entry.graphic == 0 && entry.object == 0) {
             return false;
         } else {
