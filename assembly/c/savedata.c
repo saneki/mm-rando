b4e9121a6976cd5681f766449a50b86b9d969bc5
saneki <s@neki.me>
2020-12-29 12:12:54 -0600

    Refactor Asm/C: Refactor Savedata file

--- assembly/c/savedata.c
+++ assembly/c/savedata.c
@@ -3,33 +3,33 @@
 #include "save_file.h"
 #include "z2.h"
 
-static bool is_owl_save_size(size_t size) {
+static bool IsOwlSaveSize(size_t size) {
     return size == 0x3CA0;
 }
 
 /**
  * Hook function called after some savedata has been loaded into SaveContext.
  **/
-void savedata_after_load(GlobalContext *game, Camera *camera, SaveContext *file, const u8 *buffer, size_t size) {
+void Savedata_AfterLoad(GlobalContext* ctxt, Camera* camera, SaveContext* file, const u8* buffer, size_t size) {
     // Read our struct from buffer with flash data
-    bool owl_save = is_owl_save_size(size);
-    u32 offset = SaveFile_GetFlashSectionOffset(owl_save);
-    const u8 *src = buffer + offset;
+    bool owlSave = IsOwlSaveSize(size);
+    u32 offset = SaveFile_GetFlashSectionOffset(owlSave);
+    const u8* src = buffer + offset;
     SaveFile_Read(src);
 }
 
 /**
  * Hook function called after savedata prepared (inventory & flags cleared via Song of Time).
  **/
-void savedata_after_prepare(GlobalContext *game) {
+void Savedata_AfterPrepare(GlobalContext* ctxt) {
     QuestItems_AfterSongOfTimeClear();
 }
 
 /**
  * Hook function called after writing SaveContext to buffer, which is to be written to flash.
  **/
-void savedata_after_write(u8 *buffer, SaveContext *file, size_t size, bool owl_save) {
-    u32 offset = SaveFile_GetFlashSectionOffset(owl_save);
-    u8 *dest = buffer + offset;
+void Savedata_AfterWrite(u8* buffer, SaveContext* file, size_t size, bool owlSave) {
+    u32 offset = SaveFile_GetFlashSectionOffset(owlSave);
+    u8* dest = buffer + offset;
     SaveFile_Write(dest);
 }
