fca145b0e0df8c535b81f24dcef7bc15f033df8c
saneki <s@neki.me>
2020-12-29 11:38:55 -0600

    Refactor Room file

--- assembly/src/hooks/rooms.asm
+++ assembly/src/hooks/rooms.asm
@@ -11,14 +11,14 @@
 ;   sw      ra, 0x002C (sp)
 .org 0x8012E970
     sw      ra, 0x002C (sp)
-    jal     room_before_load_hook
+    jal     Room_BeforeLoad_Hook
     sw      s0, 0x0028 (sp)
 
 ; After room load.
 ; Replaces:
 ;   jr      ra
 .org 0x8012EAA0
-    j       room_after_load_hook
+    j       Room_AfterLoad_Hook
 
 ;==================================================================================================
 ; Room Unload Hooks
@@ -31,11 +31,11 @@
 ;   sw      ra, 0x001C (sp)
 .org 0x8012EBFC
     sw      ra, 0x001C (sp)
-    jal     room_before_unload_hook
+    jal     Room_BeforeUnload_Hook
     sw      s0, 0x0018 (sp)
 
 ; After room unload.
 ; Replaces:
 ;   jr      ra
 .org 0x8012EC74
-    j       room_after_unload_hook
+    j       Room_AfterUnload_Hook
