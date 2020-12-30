da2432825a0fc6e98c210b0495d171efd20666cf
saneki <s@neki.me>
2020-12-30 01:14:16 -0600

    Refactor Asm/C: Refactor Objheap file

--- assembly/c/Objheap.c
+++ assembly/c/Objheap.c
@@ -3,60 +3,60 @@
 #include "Linheap.h"
 #include "Objheap.h"
 
-enum objheap_op {
+enum ObjheapOp {
     OP_NONE,
     OP_ADVANCE,
     OP_REVERT,
 };
 
-static void load_object_file(u32 object_id, u8 *buf) {
-    ObjectFileTableEntry *entry = ExtendedObjects_Get((s16)object_id);
-    u32 vrom_start = entry->vromStart;
-    u32 size = entry->vromEnd - vrom_start;
-    z2_ReadFile(buf, vrom_start, size);
+static void LoadObjectFile(u32 objectId, u8* buf) {
+    ObjectFileTableEntry* entry = ExtendedObjects_Get((s16)objectId);
+    u32 vromStart = entry->vromStart;
+    u32 size = entry->vromEnd - vromStart;
+    z2_ReadFile(buf, vromStart, size);
 }
 
-static size_t get_object_size(u32 object_id) {
-    ObjectFileTableEntry info = *ExtendedObjects_Get((s16)object_id);
+static size_t GetObjectSize(u32 objectId) {
+    ObjectFileTableEntry info = *ExtendedObjects_Get((s16)objectId);
     return (size_t)(info.vromEnd - info.vromStart);
 }
 
-static void objheap_item_clear(struct objheap_item *obj) {
+static void ItemClear(struct ObjheapItem* obj) {
     obj->buf = NULL;
-    obj->object_id = 0;
+    obj->objectId = 0;
     obj->room = -1;
 }
 
-static void objheap_item_load_object(struct objheap_item *object, u32 object_id) {
-    object->object_id = object_id;
-    load_object_file(object_id, object->buf);
+static void ItemLoadObject(struct ObjheapItem* object, u32 objectId) {
+    object->objectId = objectId;
+    LoadObjectFile(objectId, object->buf);
 }
 
 /**
  * Return pointer to data of object by Id tied to specific room, or allocate if necessary.
  **/
-struct objheap_item* objheap_allocate(struct objheap *heap, u32 object_id, s8 room) {
+struct ObjheapItem* Objheap_Allocate(struct Objheap* heap, u32 objectId, s8 room) {
     // Collectible actors will usually have a room value of -1. These seem to draw regardless of the current room the game has loaded.
     if (room == -1) {
         // Hackily treat as tied to the current room. When transitioning to other room, should allocate new data for that room before current data is freed.
         if (heap->op == OP_ADVANCE) {
-            room = heap->nex_room;
+            room = heap->nexRoom;
         } else {
-            room = heap->cur_room;
+            room = heap->curRoom;
         }
     }
     // Sanity check: ensure object is from relevant room.
-    if (room == heap->cur_room || room == heap->nex_room) {
+    if (room == heap->curRoom || room == heap->nexRoom) {
         // Do initial pass to check if object is already loaded for the specific room.
-        struct objheap_item *other = NULL, *unused = NULL;
+        struct ObjheapItem *other = NULL, *unused = NULL;
         for (size_t i = 0; i < heap->count; i++) {
-            struct objheap_item *obj = &heap->objs[i];
-            if (obj->object_id == object_id && obj->room == room) {
+            struct ObjheapItem* obj = &heap->objs[i];
+            if (obj->objectId == objectId && obj->room == room) {
                 return obj;
-            } else if (obj->object_id == object_id) {
+            } else if (obj->objectId == objectId) {
                 // Remember same object data allocated for other room, for memcpy if needed.
                 other = obj;
-            } else if (obj->object_id == 0 && unused == NULL) {
+            } else if (obj->objectId == 0 && unused == NULL) {
                 // Remember for allocating if object not found.
                 unused = obj;
             }
@@ -65,24 +65,24 @@ struct objheap_item* objheap_allocate(struct objheap *heap, u32 object_id, s8 ro
         // This is to ensure that we can safely revert advancing if unloading the new room first, as all new object data will be for the new room.
         // For example, going from: Old room loaded => Old & New rooms loaded => Old room loaded
         // This behavior is relevant to the room transition between Sewers and Astral Observatory.
-        if (heap->cur_room != heap->nex_room && room == heap->cur_room) {
+        if (heap->curRoom != heap->nexRoom && room == heap->curRoom) {
             return NULL;
         }
         // If unused object slot was found, use it for allocation.
         if (unused != NULL) {
-            struct objheap_item *obj = unused;
-            size_t objsize = get_object_size(object_id);
+            struct ObjheapItem* obj = unused;
+            size_t objsize = GetObjectSize(objectId);
             // Ensure heap could successfully allocate enough space.
             obj->buf = Linheap_Alloc(&heap->linheap, objsize);
             if (obj->buf != NULL) {
                 obj->room = room;
                 if (other != NULL) {
                     // Copy existing memory from loaded object data.
-                    obj->object_id = other->object_id;
+                    obj->objectId = other->objectId;
                     z2_memcpy(obj->buf, other->buf, objsize);
                 } else {
                     // Load object from file.
-                    objheap_item_load_object(obj, object_id);
+                    ItemLoadObject(obj, objectId);
                 }
                 return obj;
             }
@@ -94,9 +94,9 @@ struct objheap_item* objheap_allocate(struct objheap *heap, u32 object_id, s8 ro
 /**
  * Clear underlying Linheap and all objheap item entries.
  **/
-void objheap_clear(struct objheap *heap) {
+void Objheap_Clear(struct Objheap* heap) {
     for (size_t i = 0; i < heap->count; i++) {
-        objheap_item_clear(&heap->objs[i]);
+        ItemClear(&heap->objs[i]);
     }
     Linheap_Clear(&heap->linheap);
 }
@@ -104,12 +104,12 @@ void objheap_clear(struct objheap *heap) {
 /**
  * Invalidate all object data not in the current room.
  **/
-static void objheap_invalidate_items(struct objheap *heap) {
+static void InvalidateItems(struct Objheap* heap) {
     for (size_t i = 0; i < heap->count; i++) {
-        struct objheap_item *obj = &heap->objs[i];
+        struct ObjheapItem* obj = &heap->objs[i];
         // Invalidate object data slot if designated for previous room.
-        if (obj->room != heap->cur_room) {
-            objheap_item_clear(obj);
+        if (obj->room != heap->curRoom) {
+            ItemClear(obj);
         }
     }
 }
@@ -118,13 +118,13 @@ static void objheap_invalidate_items(struct objheap *heap) {
  * Flush heap operation, in which case the heap should either finish advance or revert advance.
  * These operations also update the relevant room indexes.
  **/
-void objheap_flush_operation(struct objheap *heap) {
+void Objheap_FlushOperation(struct Objheap* heap) {
     if (heap->op == OP_ADVANCE) {
         // Old room => Old & New rooms => New room
-        objheap_finish_advance(heap);
+        Objheap_FinishAdvance(heap);
     } else if (heap->op == OP_REVERT) {
         // Old room => Old & New rooms => Old room
-        objheap_revert_advance(heap);
+        Objheap_RevertAdvance(heap);
     }
     heap->op = OP_NONE;
 }
@@ -132,12 +132,12 @@ void objheap_flush_operation(struct objheap *heap) {
 /**
  * Handle room unload, which will determine and store which operation to perform when ready.
  **/
-void objheap_handle_room_unload(struct objheap *heap, s8 cur_room) {
+void Objheap_HandleRoomUnload(struct Objheap* heap, s8 curRoom) {
     // Update operation based on room unload.
-    if (heap->nex_room == cur_room) {
+    if (heap->nexRoom == curRoom) {
         // Advance
         heap->op = OP_ADVANCE;
-    } else if (heap->cur_room == cur_room) {
+    } else if (heap->curRoom == curRoom) {
         // Revert
         heap->op = OP_REVERT;
     } else {
@@ -149,11 +149,11 @@ void objheap_handle_room_unload(struct objheap *heap, s8 cur_room) {
 /**
  * Finish advance on underlying Linheap, and clear objheap item entries from previous room.
  **/
-void objheap_finish_advance(struct objheap *heap) {
+void Objheap_FinishAdvance(struct Objheap* heap) {
     // Advance room index.
-    heap->cur_room = heap->nex_room;
+    heap->curRoom = heap->nexRoom;
     // Invalidate items for previously-loaded room.
-    objheap_invalidate_items(heap);
+    InvalidateItems(heap);
     // Finish advance in underlying heap.
     Linheap_FinishAdvance(&heap->linheap);
 }
@@ -161,30 +161,30 @@ void objheap_finish_advance(struct objheap *heap) {
 /**
  * Initialize objheap with underlying heap buffer and provided item array.
  **/
-void objheap_init(struct objheap *heap, void *base, size_t size, struct objheap_item *objs, size_t count) {
+void Objheap_Init(struct Objheap* heap, void* base, size_t size, struct ObjheapItem* objs, size_t count) {
     heap->linheap.size = size;
     Linheap_Init(&heap->linheap, base);
     heap->objs = objs;
     heap->count = count;
     for (size_t i = 0; i < count; i++) {
-        objheap_item_clear(&objs[i]);
+        ItemClear(&objs[i]);
     }
-    heap->cur_room = heap->nex_room = -1;
+    heap->curRoom = heap->nexRoom = -1;
     heap->op = OP_NONE;
 }
 
 /**
  * Initialize current & next room values when loading scene.
  **/
-void objheap_init_room(struct objheap *heap, s8 room) {
-    heap->cur_room = heap->nex_room = room;
+void Objheap_InitRoom(struct Objheap* heap, s8 room) {
+    heap->curRoom = heap->nexRoom = room;
 }
 
 /**
  * Prepare underlying Linheap for advance, and update next room value.
  **/
-void objheap_prepare_advance(struct objheap *heap, s8 room) {
-    heap->nex_room = room;
+void Objheap_PrepareAdvance(struct Objheap* heap, s8 room) {
+    heap->nexRoom = room;
     heap->op = OP_NONE;
     Linheap_PrepareAdvance(&heap->linheap);
 }
@@ -192,11 +192,11 @@ void objheap_prepare_advance(struct objheap *heap, s8 room) {
 /**
  * Revert advance on underlying heap, and clear objheap item entries from unloaded room.
  **/
-void objheap_revert_advance(struct objheap *heap) {
+void Objheap_RevertAdvance(struct Objheap* heap) {
     // Revert room index.
-    heap->nex_room = heap->cur_room;
+    heap->nexRoom = heap->curRoom;
     // Invalidate items for previously-loaded room.
-    objheap_invalidate_items(heap);
+    InvalidateItems(heap);
     // Revert advance on underlying Linheap.
     Linheap_RevertAdvance(&heap->linheap);
 }
