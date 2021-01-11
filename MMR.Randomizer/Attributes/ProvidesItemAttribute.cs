97a1480edfad9fe5b80ac920f825c9e8879b9e3f
saneki <s@neki.me>
2021-01-10 21:20:34 -0600

    Partial decouple of Item (Location) and ProvidedItem (Item), compiles but not tested

---
+++ MMR.Randomizer/Attributes/ProvidesItemAttribute.cs
@@ -0,0 +1,15 @@
+﻿using MMR.Randomizer.GameObjects;
+using System;
+
+namespace MMR.Randomizer.Attributes
+{
+    public class ProvidesItemAttribute : Attribute
+    {
+        public ProvidedItem Item { get; }
+
+        public ProvidesItemAttribute(ProvidedItem item)
+        {
+            Item = item;
+        }
+    }
+}
