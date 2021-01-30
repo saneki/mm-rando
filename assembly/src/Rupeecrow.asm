47435241cada18cb2126f502bc30dec30f545d00
ZoeyZolotova <zoey.zolotova@gmail.com>
2021-01-30 20:15:54 +1100

    Fix TF Rupee Crow crash.

--- assembly/src/Rupeecrow.asm
+++ assembly/src/Rupeecrow.asm
@@ -1,5 +1,5 @@
 Rupeecrow_AfterRupeeSpawn_Hook:
     lw      a0, 0x003C (sp)
-    j       Rupeecrow_AfterRupeeSpawn
     or      a1, s0, r0
+    j       Rupeecrow_AfterRupeeSpawn
     or      a2, v0, r0
