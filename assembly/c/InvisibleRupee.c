8dd4d8abc25f8dc47056732f904e170ecd3bd5f1
ZoeyZolotova <zoey.zolotova@gmail.com>
2021-04-11 16:31:20 +1000

    Make invisible rupee items spawn directly on the player to mimc their vanilla behavior.

--- assembly/c/InvisibleRupee.c
+++ assembly/c/InvisibleRupee.c
@@ -4,7 +4,8 @@
 
 void InvisibleRupee_GiveItem(Actor* actor, GlobalContext* ctxt) {
     u16 type = actor->params & 0x03;
-    ActorEnItem00* item = z2_fixed_drop_spawn(ctxt, &actor->currPosRot.pos, type);
+    Vec3f* pos = &GET_PLAYER(ctxt)->base.currPosRot.pos;
+    ActorEnItem00* item = z2_fixed_drop_spawn(ctxt, pos, type);
 
     u16 flag = actor->params >> 2;
     u16 giIndex = 0x34C + flag;
